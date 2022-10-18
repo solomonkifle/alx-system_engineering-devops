5830c11071dc0137ec6b7404c2690ab65c504400
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2022-10-18 12:30:38 +0300

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-# create a manifest that kills a process named killmenow
+# kill process killmenow
 
 exec { 'pkill':
-  command  => 'pkill -f killmenow',
+  command  => 'pkill killmenow',
   provider => 'shell',
 }
