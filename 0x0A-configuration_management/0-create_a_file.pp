cc8468ca7845674cdd5c37f1faabc08c7531eb87
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2022-10-18 13:02:12 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-# creates a file in /tmp
-
+# Creates a file
 file { '/tmp/school':
-  content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet'
 }
